f655aa15566a59b62fafe1dbafa6eb06cfbb73df
Newbie9999 <ilwen@ukr.net>
2019-01-11 14:31:35 +0200

    hibernate fix try

--- src/main/java/co/norse/hr/mainservice/controller/TestController.java
+++ src/main/java/co/norse/hr/mainservice/controller/TestController.java
@@ -1,5 +1,6 @@
 package co.norse.hr.mainservice.controller;
 
+import co.norse.hr.mainservice.entity.Employee;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,17 +23,24 @@ public class TestController {
     }
 
     @GetMapping("equation")
-    public static String solve(@RequestParam double a, @RequestParam double b, @RequestParam double c){
-        double D = b*b - 4*a*c;
-        if (D<0) {
+    public static String solve(@RequestParam double a, @RequestParam double b, @RequestParam double c) {
+        double D = b * b - 4 * a * c;
+        if (D < 0) {
             return "no answer";
-        } else if (D==0) {
-            double x = -1 * b /(2*a);
-            return "answer: " + x + "<br>a = " + a + " b = " + b + " c = "+c;
+        } else if (D == 0) {
+            double x = -1 * b / (2 * a);
+            return "answer: " + x + "<br>a = " + a + " b = " + b + " c = " + c;
         } else {
-            double x1 = (-1 * b - Math.sqrt(D))/(2*a);
-            double x2 = (-1 * b + Math.sqrt(D))/(2*a);
+            double x1 = (-1 * b - Math.sqrt(D)) / (2 * a);
+            double x2 = (-1 * b + Math.sqrt(D)) / (2 * a);
             return "answer: x1 = " + x1 + "x2 = " + x2;
         }
+    }
+
+    @GetMapping("employee")
+    public static String newEmployee(@RequestParam String name) {
+        Employee employee = new Employee();
+        employee.setFirstName(name);
+        return employee.toString();
     }
 }
