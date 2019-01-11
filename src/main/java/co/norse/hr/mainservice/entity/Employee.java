9cd007c513bd6e1fa0fb1d898dd032cb455167c6
Newbie9999 <ilwen@ukr.net>
2019-01-11 13:42:27 +0200

    employee entity added

---
+++ src/main/java/co/norse/hr/mainservice/entity/Employee.java
@@ -0,0 +1,16 @@
+package co.norse.hr.mainservice.entity;
+
+public class Employee {
+    private int id;
+    private int company_id;
+    private String first_name;
+    private String last_name;
+    private int birthday;
+    private String email;
+    private String phone;
+    private String room_number;
+    private String position;
+    private String description;
+    private int office_id;
+
+}
