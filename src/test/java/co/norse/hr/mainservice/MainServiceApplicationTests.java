bef1d411fc775cdd0d4b50bb187610741cf9ec5f
Newbie9999 <ilwen@ukr.net>
2019-01-14 01:58:05 +0200

    TestController removed

--- src/test/java/co/norse/hr/mainservice/MainServiceApplicationTests.java
+++ src/test/java/co/norse/hr/mainservice/MainServiceApplicationTests.java
@@ -1,10 +1,8 @@
 package co.norse.hr.mainservice;
 
-import co.norse.hr.mainservice.controller.TestController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
