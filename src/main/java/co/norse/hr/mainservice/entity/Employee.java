91e65aae2a9c8ac9f3334682cb6832debb2f9eab
Newbie9999 <ilwen@ukr.net>
2019-01-14 01:59:35 +0200

    IDENTITY Employee.id type

--- src/main/java/co/norse/hr/mainservice/entity/Employee.java
+++ src/main/java/co/norse/hr/mainservice/entity/Employee.java
@@ -10,7 +10,7 @@ import javax.persistence.Column;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     private Company company;
