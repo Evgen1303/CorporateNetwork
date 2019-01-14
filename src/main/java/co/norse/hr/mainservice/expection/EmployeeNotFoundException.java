3422b76c5f70d379fbe5dca425fb050da43b14db
Newbie9999 <ilwen@ukr.net>
2019-01-14 02:02:35 +0200

    EmployeeNotFoundException fixed

--- src/main/java/co/norse/hr/mainservice/expection/EmployeeNotFoundException.java
+++ src/main/java/co/norse/hr/mainservice/expection/EmployeeNotFoundException.java
@@ -1,4 +1,6 @@
 package co.norse.hr.mainservice.expection;
 
-public class EmployeeNotFoundException extends NullPointerException {
+import javax.persistence.EntityNotFoundException;
+
+public class EmployeeNotFoundException extends EntityNotFoundException {
 }
