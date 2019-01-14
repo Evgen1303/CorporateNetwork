19e2d731acce69d7e5036047c9021c504bd0aedd
Newbie9999 <ilwen@ukr.net>
2019-01-14 02:00:48 +0200

    unuseful @Coloumn removed

--- src/main/java/co/norse/hr/mainservice/entity/Employee.java
+++ src/main/java/co/norse/hr/mainservice/entity/Employee.java
@@ -4,14 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Column;
 
 @Entity
 public class Employee {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
     private Company company;
     private String firstName;
